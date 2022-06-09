b740d54364321c3c0e67f32c66b7813a3aae81c2
Kacper Warda <kacper.warda@logintegra.com>
2022-06-09 19:49:03 +0200

    Pobieranie cen paliwa co 10 sekund

---
+++ grails-app/jobs/FuelPriceUpdateJob.groovy
@@ -0,0 +1,17 @@
+import szkolenie2022czerwiec.FuelCostService
+
+class FuelPriceUpdateJob {
+
+    FuelCostService fuelCostService
+
+    static triggers = {
+        simple repeatInterval: 10000 //co 1000ms = 1s
+    }
+
+    void execute() {
+        println "start of prices update process"
+        fuelCostService.updateAllFuelTypePrices()
+        println "end of prices update process"
+
+    }
+}
